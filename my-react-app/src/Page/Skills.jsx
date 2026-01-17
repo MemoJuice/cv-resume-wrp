07b4daad820a63b9f6accf46afc627b9ec1b540b
unknown <wrpxdouble1135@gmail.com>
2026-01-17 19:02:04 +0700

    addead skill success

--- my-react-app/src/Page/Skills.jsx
+++ my-react-app/src/Page/Skills.jsx
@@ -1,17 +1,21 @@
 function Skills() {
   return (
     <div className="min-h-screen">
-      <p className="text-white font-semibold text-center text-4xl mb-15 text-shadow-transparent">
-        Skills
-      </p>
-      <ul className=" bg-linear-to-r from-green-500 via-yellow-300 to-yellow-600 bg-clip-text text-transparent font-semibold text-center text-2xl flex flex-col space-y-4">
-        <li className="">React</li>
-        <li className="">JavaScript</li>
-        <li className="">Tailwind CSS</li>
-        <li className="">Node.js</li>
-        <li className="">MongoDB</li>
-        <li className="">Git</li>
-      </ul>
+      <div>
+        <p className="text-white font-semibold text-center text-4xl mb-15 text-shadow-transparent">
+          Skills
+        </p>
+      </div>
+      <div>
+        <div className=" bg-linear-to-r from-green-500 via-yellow-300 to-yellow-600 bg-clip-text text-transparent font-semibold text-2xl flex flex-wrap gap-5 text-center items-center justify-center">
+          <div className="border-2 border-white px-5 py-2 w-fill">React</div>
+          <div className="border-2 border-white px-5 py-2 w-fill">JavaScript</div>
+          <div className="border-2 border-white px-5 py-2 w-fill">Tailwind CSS</div>
+          <div className="border-2 border-white px-5 py-2 w-fill">Node.js</div>
+          <div className="border-2 border-white px-5 py-2 w-fill">MongoDB</div>
+          <div className="border-2 border-white px-5 py-2 w-fill">Git</div>
+        </div>
+      </div>
     </div>
   );
 }
