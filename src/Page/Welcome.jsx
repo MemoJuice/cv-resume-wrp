d02d18a1535ace8386a4f0a26a319d9895f47443
unknown <wrpxdouble1135@gmail.com>
2026-01-17 21:48:08 +0700

    edit litle point

--- src/Page/Welcome.jsx
+++ src/Page/Welcome.jsx
@@ -3,13 +3,13 @@ export default function TextName() {
     <div>
       <h1 className="text-6xl font-bold text-center mt-30 bg-linear-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent">
         {" "}
-        Hi. I'm Worapon.
+        Hi I'm Worapon.
       </h1>
       <p
         className=" text-center mt-4 mx-auto bg-linear-to-r from-green-400 via-blue-500 to-purple-500 bg-clip-text text-transparent
       text-4xl py-10 lg:w-2/3 font-light leading-relaxed"
       >
-        <b className="font-bold">Hello, my name is M</b> Although I did not graduate in a technology-related
+        <b className="font-bold">Hello my name is M</b> Although I did not graduate in a technology-related
         field, I have always had a strong interest in technology. Therefore, I
         began studying and developing my skills independently and continuously,
         with the goal of transitioning into a career in the technology field. I
